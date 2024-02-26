6223e9099cfb6183ca5565ff271d8508df562279
rahfflesia <154933782+rahfflesia@users.noreply.github.com>
2024-02-25 17:57:42 -0700

    coded reset canvas function

--- UI/etch-a-script.js
+++ UI/etch-a-script.js
@@ -1,14 +1,21 @@
 //Code for 16x16 Grid
 const canvas = document.querySelector('.canvas');
-for(let i = 0; i < 16; i++){
-    const row = document.createElement('div');
-    row.setAttribute('style','width:500px');
-    canvas.appendChild(row);
-    for(let j = 0; j < 16; j++){
-        const square = document.createElement('div');
-        square.setAttribute('style', 'border:solid 1.5px gray; width:31.25px; height:31.25px');
-        square.classList.add('square');
-        row.appendChild(square);
+gridSize();
+
+function gridSize(sideLength = 16){
+    let squareArea = 500 / sideLength;
+    for(let i = 0; i < sideLength; i++){
+        const row = document.createElement('div');
+        row.setAttribute('style','width:500px');
+        canvas.appendChild(row);
+        for(let j = 0; j < sideLength; j++){
+            const square = document.createElement('div');
+            square.setAttribute('style', 'border:solid 1.5px gray;');
+            square.style.width = squareArea + "px";
+            square.style.height = squareArea + "px";
+            square.classList.add('square');
+            row.appendChild(square);
+        }
     }
 }
 
@@ -23,8 +30,12 @@ canvasSquare.forEach(function(square){
 const resetCanvasButton = document.querySelector('.reset-canvas');
 
 resetCanvasButton.addEventListener('click', () => {
-    
+    canvasSquare.forEach(function(square){
+        square.style.backgroundColor = "white";
+    });
 });
+
+
 
 
 
